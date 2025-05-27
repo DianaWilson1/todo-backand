0c6452c21ba0657a2741112755254d6110e9394a
DianaWilson1 <shedteckned@gmail.com>
2025-05-27 16:45:18 -0400

    .

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const Todo = require("./models");
 dbConnect();
 const app = express();
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 app.use(morgan("dev"));
@@ -23,30 +24,51 @@ app.get("/", async (req, res) => {
 
 // get all
 app.get("/todo/all", async (req, res) => {
-  res.send({ todos: [] });
+  // call find on todo model/schema to get all todos
+  try {
+    const todos = await Todo.find();
+    res.status(200).send({ todos });
+  } catch (error) {
+    res.status(404).send(`Sorry, cant find todos ${error}`);
+  }
 });
 
 // get by id
 app.get("/todo/:id", async (req, res) => {
-  res.send('Hello world');
+  try {
+    const todos = await Todo.findById(req.params.id);
+    res.status(200).send({ todos });
+  } catch (error) {
+    res.status(404).send(`Sorry, cant find todos ${error}`);
+  }
 });
 
 // create
 app.post("/todo", async (req, res) => {
   const { title, description, isComplete } = req.body;
-  const todo = await Todo.createTodo({ title, description, isComplete });
+  const todo = await Todo.create({title, description, isComplete});
 
   res.send(todo);
 });
 
 // edit
 app.put("/todo/:id", async (req, res) => {
-  res.send('Hello world');
+  try {
+    const todos = await Todo.findOneAndUpdate(req.body.id, req.body);
+    res.status(200).send({ todos });
+  } catch (error) {
+    res.status(404).send(`Sorry, cant find todos ${error}`);
+  }
 });
 
 // delete
-app.delete("todo/:id", async (req, res) => {
-  res.send('Hello world');
+app.delete("/todo/:id", async (req, res) => {
+  try {
+    const todos = await Todo.findByIdAndDelete(req.params.id);
+    res.status(200).send({ todos });
+  } catch (error) {
+    res.status(404).send(`Sorry, cant find todos ${error}`);
+  }
 });
 
 
